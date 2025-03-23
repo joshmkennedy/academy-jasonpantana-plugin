7a3a843423a2356527a2099339234df1742c0699
josh kennedy <jkennedy@ksensetech.com>
2025-03-23 11:01:16 -0400

    track vendor files now

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joshkennedy/jp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '140e3347d68acbd0b27ff9291df1a8065e4e5aae',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joshkennedy/jp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '140e3347d68acbd0b27ff9291df1a8065e4e5aae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.1.6',
+            'version' => '2.1.6.0',
+            'reference' => '6eaec7c6c9e90dcfe46ad1e1ffa5171e2dab641c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
