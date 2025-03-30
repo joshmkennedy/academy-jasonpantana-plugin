0baca05061662cb934882823a0a3ca0498e78f59
josh kennedy <jkennedy@ksensetech.com>
2025-03-30 12:20:44 -0400

    adds user group to endpoint

--- userbyemail.php
+++ userbyemail.php
@@ -6,9 +6,9 @@ add_action('rest_api_init', function () {
         'methods' => 'POST',
         'callback' => 'jp_usersbyemail',
         'permission_callback' => function () {
-        // Restrict who can use this endpoint as necessary
-        // For example, only logged-in users who can list users:
-           return current_user_can('list_users');
+            // Restrict who can use this endpoint as necessary
+            // For example, only logged-in users who can list users:
+            return current_user_can('list_users');
         },
 
     ));
@@ -21,9 +21,16 @@ function jp_usersbyemail(WP_REST_Request $request): WP_REST_Response {
     foreach ($emails as $email) {
         $user = get_user_by('email', $email);
         if ($user) {
-            $users[$email] = [$user->first_name, $user->last_name];
+            $learnDashGroup = getLearndashGroups($user);
+            $users[$email] = [$user->first_name, $user->last_name, $learnDashGroup];
         }
     }
 
     return rest_ensure_response($users);
+}
+
+function getLearndashGroups($user) {
+    $groupIds = array_filter(\learndash_get_users_group_ids($user->ID), fn($id) => isPaidGroup($id));
+    $groupTitles=  array_map(fn($gid) => get_post($gid)?->post_title ?? "unknown", $groupIds);
+    return implode(", ", $groupTitles);
 }
